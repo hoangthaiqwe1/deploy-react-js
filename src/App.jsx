37615cb1906077af7adb4124de848e2e7237c558
hoangthaiqwe1 <thaidang11041104@gmail.com>
2023-12-30 16:23:49 +0700

    Init

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HomeComponent from "./Component/HomeComponent/HomeComponent";
 import IntroduComponent from "./Component/IntroduComponent/IntroduComponent";
 import ProductComponent from "./Component/ProductComponent/ProductComponent";
 import ContactComponent from "./Component/ContactComponent/ContactComponent";
+import NotFoundComponent from "./Component/NotFoundComponent/NotFoundComponent";
 import DetailComponent from "./Component/DetailComponent/DetailComponent";
 import AppContext from "./utils/AppContext";
 import CartComponent from "./Component/CartComponent/CartComponent";
@@ -36,6 +37,7 @@ function App() {
               element={<DetailComponent />}
             />
             <Route path="/deploy-react-js/cart" element={<CartComponent />} />
+            <Route path="*" element={<NotFoundComponent />} />
           </Route>
         </Routes>
       </AppContext>
